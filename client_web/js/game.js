f5da4f202ce88c697d99f6f612806e0db74bad3e
nath54 <cerisara.nathan@gmail.com>
2020-10-14 16:08:59 +0200

    aa

---
+++ client_web/js/game.js
@@ -0,0 +1,32 @@
+
+
+//fonction qui ajoute du texte 
+function aff_message(txt,color="white"){
+    //on crée l'element
+    var p = document.createElement("pre");
+    p.innerHTML=txt;
+    p.setAttribute("class","mes")
+    p.style.color=color;
+    //on l'ajoute 
+    document.getElementById("mm").appendChild(p);
+    document.getElementById("messages").scrollTop = document.getElementById("messages").scrollHeight;
+}
+
+
+
+function init(){
+    for(x=0; x<100; x++){
+        aff_message("a "+x);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
