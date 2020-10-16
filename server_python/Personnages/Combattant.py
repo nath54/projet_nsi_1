75590d5b1f35c91ea57b9efe329d8d310b1fb1cb
Er1t-h <62598471+er1t-h@users.noreply.github.com>
2020-10-16 10:07:43 +0200

    Update

--- server_python/Personnages/Combattant.py
+++ server_python/Personnages/Combattant.py
@@ -3,14 +3,14 @@ import server_python.Personnages.Etre
 
 class Combattant(Etre):
     """Classe abstraite dont dérivera les personnages pouvant se battre
-    
+
     Attributes:
         vie_totale(int): Vie maximale
         vie(int): Vie restante
         energie_totale(int): Énergie maximale
         energie(int): Énergie restante
         effets(list): Effets appliqués sur l'instance du combattant
-    
+
     """
     def __init__(self):
         pass
