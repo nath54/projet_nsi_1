7f82daa63491ba32e1fb13faa92c016f7b7fde82
nath54 <cerisara.nathan@gmail.com>
2020-10-17 19:13:07 +0200

    Work on Client Web

--- client_web/js/game.js
+++ client_web/js/game.js
@@ -1,22 +1,38 @@
 
 
 //fonction qui ajoute du texte
-function aff_message(txt,color="white"){
+function aff_message(txt,color="white",text_on_click=null){
     //on crée l'element
-    var p = document.createElement("pre");
-    p.innerHTML=txt;
-    p.setAttribute("class","mes")
-    p.style.color=color;
-    //on l'ajoute
-    document.getElementById("mm").appendChild(p);
+    if(text_on_click!=null){
+        var p = document.createElement("pre");
+        p.innerHTML=txt;
+        p.setAttribute("class","mes")
+        p.style.color=color;
+        var s = document.createElement("span");
+        s.appendChild(p);
+        s.setAttribute("onclick",'text_click("'+text_on_click+'");');
+        //on l'ajoute
+        document.getElementById("mm").appendChild(s);
+    }
+    else{
+        var p = document.createElement("pre");
+        p.innerHTML=txt;
+        p.setAttribute("class","mes")
+        p.style.color=color;
+        //on l'ajoute
+        document.getElementById("mm").appendChild(p);
+    }
     document.getElementById("messages").scrollTop = document.getElementById("messages").scrollHeight;
 }
 
 function init(){
+    aff_message("click",color="blue",text_on_click="This is blue !")
+    aff_message("click",color="white",text_on_click="This is white !")
+    aff_message("click",color="red",text_on_click="This is red !")
 }
 
 function text_click(text){
-    document.getElementById("input").value+=text;
+    document.getElementById("input").value+=" "+text;
 }
 
 
