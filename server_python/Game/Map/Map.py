78354e2d1c11a7626f8e24928ecbc80b079da99a
nathan cerisara <cerisara.nathan@gmail.com>
2020-10-21 14:08:24 +0200

    pas grand chose

--- server_python/Game/Map/Map.py
+++ server_python/Game/Map/Map.py
@@ -1,5 +1,22 @@
+import io,json,os
+
 class Map:
     """Classe qui gérera la map du jeu"""
     def __init__(self):
         # TODO
         pass
+
+    def load_from_json(self):
+        """
+
+        """
+        emplacement="Data/map/"
+        for fichier in os.listdir(emplacement):
+            f=io.open(emplacement+fichier,"r",encoding="utf-8")
+            data=json.loads(f.read())
+            print(data)
+            f.close()
+
+if __name__=="__main__":
+    map=Map()
+    map.load_from_json()
