d0d2f1192fdc563f96031bed967d6d028e238b58
ExTer3012 <72519253+ExTer3012@users.noreply.github.com>
2020-11-02 20:05:48 +0100

    cheat code

---
+++ server_python/Data/pnjs/cheat code .py
@@ -0,0 +1,20 @@
+def cheat code ():
+    if commande == "sonto":
+        game.pnj (le pére noel donne un cadeau)
+    if commande == "docteur":
+        game.objets (du soin)
+    if commande == "666"
+        print ("vous avez passer un pacte avec le diable")
+        game.objets
+    if commande == "Excalibur":
+        game.objets (une épée)
+    if commande == "Merlin"
+        game.objets (une baguette)
+    if commande == "Trump":
+        game.objets (beaucoup d'argent)
+    if commande == "brevis":
+        game.pnj (une brebis)
+    ...
+    else:
+        print("non mais ca va pas, ce n'est pas bien de tricher")
+    
